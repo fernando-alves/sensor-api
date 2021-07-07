3198cb2c9181570c5d5067d66565d3d2ae270ad6
Fernando <fernandoasfj+github@gmail.com>
2021-07-07 19:18:22 +0200

    add optional basic auth

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const storage = require('./lib/storage')
+const auth = require('./lib/auth')
 
 app.use(express.json());
 
@@ -9,11 +10,11 @@ app.get('/', (req, res) => {
   res.send('up')
 })
 
-app.get('/readings', (req, res) => {
+app.get('/readings', auth, (req, res) => {
   storage.loadAll().then(readings => res.send(readings))
 })
 
-app.post('/readings', (req, res) => {
+app.post('/readings', auth, (req, res) => {
   storage.store(req.body).then(() => res.status(201).end())
 })
 
